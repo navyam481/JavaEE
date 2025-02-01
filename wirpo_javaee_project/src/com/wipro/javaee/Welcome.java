64faa4cb79dfe317fdf160ab6e86d2892d94293c
M NAVYASREE <navyam481@gmail.com>
2025-02-01 10:03:29 +0530

    commit

---
+++ wirpo_javaee_project/src/com/wipro/javaee/Welcome.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package com.wipro.javaee;
+
+/**
+ * 
+ */
+public class Welcome {
+
+	/**
+	 * 
+	 */
+	public Welcome() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
