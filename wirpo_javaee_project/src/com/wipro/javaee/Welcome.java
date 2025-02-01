b98152e82ab3f3232ee911aa9ff8bb77acd708c8
M NAVYASREE <navyam481@gmail.com>
2025-02-01 12:12:23 +0530

    commit it

--- wirpo_javaee_project/src/com/wipro/javaee/Welcome.java
+++ wirpo_javaee_project/src/com/wipro/javaee/Welcome.java
@@ -19,7 +19,7 @@ public class Welcome {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("Hello World!!");
+		System.out.println("Hello World!! Welcome");
 
 	}
 
